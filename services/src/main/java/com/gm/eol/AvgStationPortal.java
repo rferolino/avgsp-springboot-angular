1f43325c76233cd582b7b901362eb26990387df7
Rolly Ferolino <rferolino@jaredsoft.com>
2020-10-05 23:01:02 -0700

    First Commit

---
+++ services/src/main/java/com/gm/eol/AvgStationPortal.java
@@ -0,0 +1,13 @@
+package com.gm.eol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvgStationPortal {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvgStationPortal.class, args);
+    }
+
+}
